f63e41bf65962040daa3cbd156a34b0465a75632
Pavlo Khodakivskyi1 <Pavlo_Khodakivskyi1@epam.com>
2024-02-27 16:08:43 +0200

    fix save in db

--- src/main/java/com/pahod/music/resourceservice/entity/AudioResourceEntity.java
+++ src/main/java/com/pahod/music/resourceservice/entity/AudioResourceEntity.java
@@ -5,11 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "audio_resource")
 public class AudioResourceEntity {
 
